0326239daddeeff5746f1f7b6a86e40018b5abbd
conserver123 <sanchezmaronharvey@gmail.com>
2023-12-09 17:51:30 +0800

    new local settings

--- student_evaluator/settings.py
+++ student_evaluator/settings.py
@@ -122,3 +122,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+try:
+    from .local_settings import *
+except ImportError:
+    print("Looks like there is no local file. You must be on production")
